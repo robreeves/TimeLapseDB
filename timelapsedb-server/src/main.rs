d2758be7499c1811c05d0ca8298e3f752dddbc32
Rob Reeves <robertpreeves@gmail.com>
2026-09-15 21:04:36 -0700

    added writer struct new

--- timelapsedb-server/src/main.rs
+++ timelapsedb-server/src/main.rs
@@ -1,3 +1,9 @@
+mod storage;
+
+use crate::storage::sst_writer::SSTWriter;
+
 fn main() {
     println!("Hello, world!");
+
+    let writer = SSTWriter::new();
 }
--- timelapsedb-server/src/storage/sst_writer.rs
+++ timelapsedb-server/src/storage/sst_writer.rs
@@ -1,1 +1,10 @@
-struct SSTWriter {}
+const HEADER_MAGIC: [u8; 12] = *b"_TLDB-START_";
+const FOOTER_MAGIC: [u8; 10] = *b"_TLDB-END_";
+
+pub struct SSTWriter {}
+
+impl SSTWriter {
+    pub fn new() -> Self {
+        SSTWriter {  }
+    }
+}
