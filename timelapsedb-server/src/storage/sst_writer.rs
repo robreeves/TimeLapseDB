2e23e2a4bccb842bd2a164ca1a589fffbf68701a
Rob Reeves <robertpreeves@gmail.com>
2026-09-17 16:15:49 -0700

    write header and footer

--- timelapsedb-server/src/storage/sst_writer.rs
+++ timelapsedb-server/src/storage/sst_writer.rs
@@ -1,9 +1,10 @@
 use std::fs::{File, OpenOptions};
-use std::io;
+use std::io::{self, BufWriter, Write};
 use std::path::PathBuf;
 
 const HEADER_MAGIC: [u8; 12] = *b"_TLDB-START_";
 const FOOTER_MAGIC: [u8; 10] = *b"_TLDB-END_";
+const VERSION: [u8; 1] = *b"0";
 
 pub struct SSTWriter {}
 
@@ -14,20 +15,22 @@ impl SSTWriter {
 
     pub fn write(&self, path: &PathBuf) -> Result<(), io::Error> {
         let file = OpenOptions::new().write(true).create_new(true).open(path)?;
+        let mut writer = BufWriter::new(file);
 
         // TODO write SST to disk
-        self.write_header(&file)?;
-        self.write_footer(&file)?;
+        self.write_header(&mut writer)?;
+        self.write_footer(&mut writer)?;
         Ok(())
     }
 
-    fn write_header(&self, file: &File) -> Result<(), io::Error> {
-        // TODO
+    fn write_header(&self, file: &mut BufWriter<File>) -> Result<(), io::Error> {
+        file.write_all(&HEADER_MAGIC)?;
+        file.write_all(&VERSION)?;
         Ok(())
     }
 
-    fn write_footer(&self, file: &File) -> Result<(), io::Error> {
-        // TODO
+    fn write_footer(&self, file: &mut BufWriter<File>) -> Result<(), io::Error> {
+        file.write_all(&FOOTER_MAGIC)?;
         Ok(())
     }
 }
