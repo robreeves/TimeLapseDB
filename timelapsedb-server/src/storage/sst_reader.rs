99cab1d3b14f5434eaaac4a2c4b92a4a21be407c
Rob Reeves <robertpreeves@gmail.com>
2026-09-18 16:53:10 -0700

    working on sst reader init

--- timelapsedb-server/src/storage/sst_reader.rs
+++ timelapsedb-server/src/storage/sst_reader.rs
@@ -1,9 +1,24 @@
-use std::path::PathBuf;
+use std::io::{self, BufReader};
+use std::{fs::File, path::PathBuf};
 
-pub struct SSTReader {}
+pub struct SSTReader {
+    reader: BufReader<File>,
+    version: u8,
+}
 
 impl SSTReader {
-    pub fn new(path: &PathBuf) -> Self {
-        SSTReader {  }
+    pub fn new(path: &PathBuf) -> Result<Self, io::Error> {
+        let file = File::open(path)?;
+        let sst_reader = SSTReader {
+            reader: BufReader::new(file),
+            version: 0
+        };
+        sst_reader.initialize()?;
+        Ok(sst_reader)
+    }
+
+    fn initialize(&self) -> Result<(), io::Error> {
+        // TODO set struct fields
+        Ok(())
     }
 }
