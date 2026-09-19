08b8b22d80782a085f357149fdb6ff64f9b007d7
Rob Reeves <robertpreeves@gmail.com>
2026-09-18 21:40:37 -0700

    read header

--- timelapsedb-server/src/storage/sst_reader.rs
+++ timelapsedb-server/src/storage/sst_reader.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufReader};
+use std::io::{self, BufReader, Read};
 use std::{fs::File, path::PathBuf};
 use super::constants::{SST_HEADER_MAGIC, SST_FOOTER_MAGIC, SST_VERSION};
 
@@ -10,7 +10,7 @@ pub struct SSTReader {
 impl SSTReader {
     pub fn new(path: &PathBuf) -> Result<Self, io::Error> {
         let file = File::open(path)?;
-        let sst_reader = SSTReader {
+        let mut sst_reader = SSTReader {
             reader: BufReader::new(file),
             version: 0
         };
@@ -18,14 +18,22 @@ impl SSTReader {
         Ok(sst_reader)
     }
 
-    fn initialize(&self) -> Result<(), io::Error> {
+    fn initialize(&mut self) -> Result<(), io::Error> {
         // TODO set struct fields
+        self.read_header()?;
         Ok(())
     }
 
-    fn read_header(&self) -> Result<(), io::Error> {
+    fn read_header(&mut self) -> Result<(), io::Error> {
         let mut magic = [0u8; SST_HEADER_MAGIC.len()];
-        //TODO
+        self.reader.read_exact(&mut magic)?;
+        println!("header magic: {}", String::from_utf8_lossy(&magic));
+
+        let mut version = [0u8; SST_VERSION.len()];
+        self.reader.read_exact(&mut version)?;
+        println!("header version: {}", String::from_utf8_lossy(&version));
+        
+        //TODO validate
 
         Ok(())
     }
